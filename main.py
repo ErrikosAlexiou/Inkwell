8086a8a14308f703a2294034f0b07cd1ec2ddaf5
Errikos Alexiou <errikos.alexiou@gmail.com>
2023-05-06 17:29:54 +0100

    sorted imports

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # importing required modules
+import os
+import subprocess
+import time
 import tkinter as Tk
+import webbrowser
+from tkinter import ttk
+
 import customtkinter as CTk
 from win10toast import ToastNotifier
-import subprocess
-import webbrowser
-import time
-import os
 
 from screenSize import ScreenSize
 
