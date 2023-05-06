fb20047fc6826f9f8c105f1a810984a341c0c7e4
Errikos Alexiou <errikos.alexiou@gmail.com>
2023-05-06 20:05:40 +0100

    advancements in development

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ root = Tk.Tk()
 
 root.title("Inkwell")
 
+
 # Calculate the size of the screen
 screenWidth = root.winfo_screenwidth()
 screenHeight = root.winfo_screenheight()
@@ -24,34 +25,35 @@ screenHeight = root.winfo_screenheight()
 appGeometry = ScreenSize(screenWidth, screenHeight, 0.8, 0.8)
 root.geometry(appGeometry)
 
-# root.configure(bg="Black")
+root.configure(bg="Black")
 
 tabControl = CTk.CTkTabview(root)
-tabControl.grid()
+tabControl.pack(expand=1, fill="both")
+
 tab1 = Tk.Frame(tabControl)
-tab1.grid()
 tab2 = Tk.Frame(tabControl)
-
-
 tabControl.add("Home")
 tabControl.add("Editor")
 
+leftFrame = CTk.CTkFrame(
+    tabControl.tab("Editor"), fg_color="red", width=100, height=100
+)
+leftFrame.grid(column=0, row=0)
 
-frame_top = CTk.CTkFrame(tab1, fg_color="aquamarine", height=100)
-frame_top.grid()
+middleFrame = CTk.CTkFrame(
+    tabControl.tab("Editor"), fg_color="green", width=100, height=90
+)
+middleFrame.grid(column=1, row=0)
 
-frame_left = CTk.CTkFrame(tab1, fg_color="red", width=200)
-frame_left.grid()
+middleBottomBar = CTk.CTkFrame(
+    tabControl.tab("Editor"), fg_color="white", width=100, height=10
+)
+middleBottomBar.grid(column=1, row=1)
 
-frame_middle = CTk.CTkFrame(tab1, fg_color="green")
-
-frame_top_inside = CTk.CTkFrame(frame_middle, fg_color="orange", height=50)
-frame_top_inside.grid()
-
-frame_middle.grid()
-
-frame_right = CTk.CTkFrame(tab1, fg_color="blue", width=200)
-frame_right.grid()
+rightFrame = CTk.CTkFrame(
+    tabControl.tab("Editor"), fg_color="blue", width=100, height=100
+)
+rightFrame.grid(column=2, row=0)
 
 
 root.mainloop()
